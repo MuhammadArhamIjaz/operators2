275e0f0067cb2daa5fd8652767c2c167966875f7
MuhammadArhamIjaz <m.arham.ijaz@gmail.com>
2025-07-10 20:47:02 +0700

    Add files via upload

---
+++ OPerators2.py
@@ -0,0 +1,11 @@
+a=23
+b=12
+c=34
+if(a > 0 or b > 0):
+  print("Either number is greater than 0")
+else:
+  print("No number is greater than 0")
+if(b> 0 or c > 0):
+  print("Either number is greater than 0")
+else:
+  print("No number is greater than 0")
